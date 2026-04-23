f1d9a82709040454d4f641e3e7611127f6950ba7
Timmy <timothy.thomas2011@hotmail.com>
2026-04-23 09:55:35 +0100

    Delete codex-auth-automation directory

--- codex-auth-automation/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { GmailReader, GmailReaderError } from './gmail-reader.js';
-export { OAuthFlowError, fullBrowserFlow, browserSignup } from './oauth-flow.js';
-export { TokenStore, TokenStoreError } from './token-store.js';
-export { CodexAuthAutomation } from './automation.js';
